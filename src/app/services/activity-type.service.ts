01589a07a9a6be881135ae7412ab1732c5771354
David Gray <dogray77@gmail.com>
2022-07-14 14:15:11 -0600

    add activity type service function

--- src/app/services/activity-type.service.ts
+++ src/app/services/activity-type.service.ts
@@ -1,10 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ActivityType } from '../ActivityType';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivityTypeService {
+  private apiUrl = 'http://localhost:9000/activity_types'
 
   constructor(private http: HttpClient) { }
+
+  getActivityTypes(): Observable<ActivityType[]> {
+    return this.http.get<ActivityType[]>(this.apiUrl)
+  }
 }
