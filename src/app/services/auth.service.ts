e6f332284f38263395425ae5ee7420f3a4ddc72e
David Gray <dogray77@gmail.com>
2022-09-02 15:41:29 -0600

    update auth service with appropriate url

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,20 +8,18 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  url = "https://activitytracker-21247-default-rtdb.firebaseio.com"
+  url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key="
   key = "AIzaSyBLi98jytycQ2mbV9s_Xrpj-j8HZQOcn_A"
 
   register(email: string, password: string) {
-    return this.http.post<AuthResponseData>(`${this.url}/${this.key}`,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    }
+    return this.http.post<AuthResponseData>(`${this.url}${this.key}`,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
     )
   }
-
-
 }
 
 export interface AuthResponseData {
