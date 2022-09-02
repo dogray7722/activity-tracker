19e4c5525a5239231ac76473b9010644d5a11cfb
David Gray <dogray77@gmail.com>
2022-09-02 11:13:45 -0600

    adds login item to menu

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  registerForm = this.fb.group({
+    username: ['', Validators.required],
+    email: ['', Validators.email],
+    password: ['', Validators.required],
+    confirmation: ['', Validators.required]
+  })
+
+  onSubmitLogin() {
+    console.log(this.loginForm.value)
+  }
+
+  onSubmitRegister() {
+    console.log(this.registerForm.value)
   }
 
 }
