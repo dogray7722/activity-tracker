04c536e1a836527f340bda5f79fb6f8db2bb5a04
David Gray <dogray77@gmail.com>
2022-09-08 14:51:16 -0600

    adds autologin functionality

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -59,6 +59,28 @@ export class AuthService {
     )
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+
+    } = JSON.parse(localStorage.getItem('userData'))
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email, 
+      userData.id, 
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    )
+    if (loadedUser.token) {
+      this.user.next(loadedUser)
+    }
+  }
+
   login(email: string, password: string) {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBLi98jytycQ2mbV9s_Xrpj-j8HZQOcn_A`
     return this.http.post<AuthResponseData>(url,
@@ -104,6 +126,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user))
   }
 
 }
