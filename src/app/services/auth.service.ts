b793f608f8df2490422101c307ae6800d1f62619
David Gray <dogray77@gmail.com>
2022-09-08 20:17:34 -0600

    adds token expiration logout logic

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -19,7 +19,8 @@ interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
-  user = new BehaviorSubject<User>(null)
+  user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient,
               private snackBarService: SnackBarService,
@@ -78,6 +79,8 @@ export class AuthService {
     )
     if (loadedUser.token) {
       this.user.next(loadedUser)
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+      this.autoLogout(expirationDuration)
       this.router.navigate(['/events'])
     }
   }
@@ -118,17 +121,28 @@ export class AuthService {
     ))
   }
 
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout()
+    }, expirationDuration);
+  }
+
   logout() {
     this.user.next(null)
     this.router.navigate(['/'])
+    localStorage.removeItem('userData')
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer)
+    }
+    this.tokenExpirationTimer = null
   }
 
   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn *1000)
     localStorage.setItem('userData', JSON.stringify(user))
   }
-
 }
 
