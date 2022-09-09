6768a33d93921c8625f3dde4fc6fc5e998250f38
David Gray <dogray77@gmail.com>
2022-09-08 21:48:13 -0600

    updates auth guard

--- src/app/components/auth/auth.guard.ts
+++ src/app/components/auth/auth.guard.ts
@@ -7,7 +7,9 @@ import { AuthService } from "src/app/services/auth.service";
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService,
               private router: Router) {}
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): 
+    boolean | UrlTree | 
+    Observable<boolean | UrlTree> {
     return this.authService.user.pipe(map(user => {
       const isLoggedIn = !!user;
       if (isLoggedIn) {
