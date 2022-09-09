7cec57759a30f583b3fdf833cd844b7d9dd656ac
David Gray <dogray77@gmail.com>
2022-09-08 21:32:28 -0600

    redirect via url tree

--- src/app/components/auth/auth.guard.ts
+++ src/app/components/auth/auth.guard.ts
@@ -1,14 +1,19 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable, map } from "rxjs";
 import { AuthService } from "src/app/services/auth.service";
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+              private router: Router) {}
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     return this.authService.user.pipe(map(user => {
-      return !!user;
+      const isLoggedIn = !!user;
+      if (isLoggedIn) {
+        return true
+      }
+      return this.router.createUrlTree(['/'])
     }))
   }
 }
