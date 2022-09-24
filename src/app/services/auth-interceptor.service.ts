3e63656feb02891e1ac382b24bcfc107b5a64e2d
David Gray <dogray77@gmail.com>
2022-09-24 15:50:21 -0600

    make sure all services are available

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -3,7 +3,9 @@ import { Injectable } from '@angular/core';
 import { exhaustMap, take } from 'rxjs';
 import { AuthService } from './auth.service';
 
-@Injectable()
+@Injectable(
+  {providedIn: 'root'}
+)
 export class AuthInterceptorService implements HttpInterceptor {
   constructor(private authService: AuthService) {}
 
